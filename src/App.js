b39f54fc26c1a2c60b8258e9b83dd982ed57875c
Mr-Forder <colforder@gmail.com>
2022-03-30 13:54:48 +0100

    used layout prop to auto animate on layout change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
-
+import { motion } from "framer-motion";
+import { useState } from "react";
 function App() {
-  return <div className="App">Hi</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="App">
+      <motion.div
+        layout
+        className="card"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <motion.h2 layout>Welcome, mateyboy</motion.h2>
+        {isOpen && (
+          <motion.div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              laboriosam a nobis rerum dolore! Voluptas corrupti quod quis
+              debitis officiis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum,
+              rem.
+            </p>
+          </motion.div>
+        )}
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
