83abd8729e0a0654e1f1e115e156c83b885d4df3
Mr-Forder <colforder@gmail.com>
2022-03-30 15:38:30 +0100

    fixed framer anim timings

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
   return (
     <div className="App">
       <motion.div
+        transition={{ layout: { type: "spring" } }}
         layout
         className="card"
+        style={{ borderRadius: "2rem" }}
         onClick={() => {
           setIsOpen(!isOpen);
         }}
       >
-        <motion.h2 layout>Welcome, mateyboy</motion.h2>
+        <motion.h2 layout="position">Welcome, mateyboy</motion.h2>
         {isOpen && (
-          <motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="expand"
+          >
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
               laboriosam a nobis rerum dolore! Voluptas corrupti quod quis
@@ -23,6 +29,7 @@ function App() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum,
               rem.
             </p>
+            <button>Submit</button>
           </motion.div>
         )}
       </motion.div>
